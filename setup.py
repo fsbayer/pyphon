608d3db3ecc1658825fa07d3707e0d6d2584057a
Frederic Bayer <fsbayer@users.noreply.github.com>
2021-07-25 04:29:36 +0100

    Prepare setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyphon',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/fsbayer/pyphon',
+    license='GPL-3.0',
+    author='Frederic Bayer',
+    author_email='',
+    description='Python sound change applier',
+    install_requires=[
+        # Phodic
+        'phodic @ git+https://git@github.com/fsbayer/phodic.git'
+    ]
+)
