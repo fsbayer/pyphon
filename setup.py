812827d55636fac16394392b05b8e00c2076f2e0
Frederic Bayer <fsbayer@users.noreply.github.com>
2021-07-25 19:22:26 +0100

    Refactor

--- setup.py
+++ setup.py
@@ -1,17 +1,33 @@
 from setuptools import setup, find_packages
-# The following is just some junk to generate dependencies
-import phondler as libphondler
-langs = libphondler.phondler.LANGUAGES
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
-    name='pyphon',
+    name='pyphon-fsbayer',
     version='0.0.1',
-    packages=find_packages(),
-    url='https://github.com/fsbayer/pyphon',
     license='GPL-3.0',
     author='Frederic Bayer',
-    author_email='',
+    author_email='frederic.s.bayer@gmail.com',
     description='Python sound change applier',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/fsbayer/pyphon',
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    package_dir={"": "\\"},
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research"
+    ],
+    python_requires=">=3.9",
     install_requires=[
         # Phondler
         'phondler @ git+https://git@github.com/fsbayer/phondler.git'
